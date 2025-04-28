3e0a0596c4d5ad9de9fce1efcc63ac4a35d1e38e
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-04-28 21:00:21 +0330

    Add messages module and enhance purchase flow tests in vitest-examples.ts and vitest-examples.test.ts, including new examples for message retrieval with spies and mocks.

--- src/mocking/vitest-examples.test.ts
+++ src/mocking/vitest-examples.test.ts
@@ -1,7 +1,8 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
-import { purchase } from "./vitest-examples";
+import { messages, purchase } from "./vitest-examples";
 
-describe.only("purchase flow", () => {
+// Example 1
+describe("purchase flow", () => {
   beforeEach(() => {
     vi.useFakeTimers();
   });
@@ -20,5 +21,46 @@ describe.only("purchase flow", () => {
     vi.setSystemTime(new Date(2025, 3, 1, 18));
     const result = purchase();
     expect(result).toEqual({ message: "Error" });
+  });
+});
+
+// Example 2
+describe("reading messages", () => {
+  it("should return the latest message with a spy", () => {
+    const spy = vi.spyOn(messages, "getLatestMessage");
+
+    expect(spy.getMockName()).toEqual("getLatestMessage");
+
+    expect(messages.getLatestMessage()).toEqual(
+      messages.items[messages.items.length - 1]
+    );
+
+    expect(spy).toHaveBeenCalledTimes(1);
+
+    spy.mockImplementationOnce(() => ({
+      body: "access-restricted",
+      from: "system",
+    }));
+    expect(messages.getLatestMessage()).toEqual({
+      body: "access-restricted",
+      from: "system",
+    });
+
+    expect(spy).toHaveBeenCalledTimes(2);
+  });
+
+  it("should return the latest message with a mock", () => {
+    const mock = vi.fn().mockImplementation(messages.getLatestMessage);
+
+    expect(mock()).toEqual(messages.items[messages.items.length - 1]);
+    expect(mock).toHaveBeenCalledTimes(1);
+
+    mock.mockImplementationOnce(() => "access-restricted");
+    expect(mock()).toEqual("access-restricted");
+
+    expect(mock).toHaveBeenCalledTimes(2);
+
+    expect(mock()).toEqual(messages.items[messages.items.length - 1]);
+    expect(mock).toHaveBeenCalledTimes(3);
   });
 });
--- src/mocking/vitest-examples.ts
+++ src/mocking/vitest-examples.ts
@@ -1,3 +1,6 @@
+// Mocking
+
+// Example 1
 const businessHours = [9, 17];
 
 export function purchase() {
@@ -9,4 +12,18 @@ export function purchase() {
   }
 
   return { message: "Error" };
+}
+
+// Example 2
+export const messages = {
+  items: [
+    { body: "Hello", from: "John" },
+    { body: "Hello again", from: "Jane" },
+    { body: "how are you?", from: "John" },
+  ],
+  getLatestMessage,
+};
+
+function getLatestMessage(index = messages.items.length - 1) {
+  return messages.items[index];
 }
